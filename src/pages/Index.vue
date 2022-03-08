544a0fa129845af04c416943ad0fdacc7a5298a6
jose-rocha <jose.rocha@hotmail.com>
2022-03-08 19:24:06 -0300

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="row" style="height: 100%;">
+    <Home/>
+  </q-page>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+import Home from './home/Home.vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: { Home },
+  setup() {
+    return { };
+  },
+});
+</script>
